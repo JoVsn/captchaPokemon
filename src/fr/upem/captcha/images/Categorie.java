6b29d7fd3a7e6ec0777724d225f29ade1cc6afb1
Jordan Vilsaint <vilsaint.jordan@gmail.com>
2018-05-17 21:35:05 +0200

    boolean URL not ok

--- src/fr/upem/captcha/images/Categorie.java
+++ src/fr/upem/captcha/images/Categorie.java
@@ -1,17 +1,36 @@
 package fr.upem.captcha.images;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Categorie implements Images{
 
 	public List<URL> getPhotos() {
+		ArrayList<URL> newList = new ArrayList<URL>();
 		
+		return newList;
 	}
 	
-	public List<URL> getRandomPhotosURL(int n);
+	public List<URL> getRandomPhotosURL(int n) {
+		ArrayList<URL> newList = new ArrayList<URL>();
+		
+		return newList;
+	}
 	
-	public URL getRandomPhotoURL();
+	public URL getRandomPhotoURL() {
+		try {
+			URL newUrl = new URL("Test");
+			return newUrl;
+		}
+
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+			return new URL("Test");
+		}
+	}
 	
-	public boolean isPhotoCorrect(URL url);
+	public boolean isPhotoCorrect(URL url) {
+		return false;
+	}
 }
